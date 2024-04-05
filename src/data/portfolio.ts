772c27a7cdcc0925bd5488f5badfba4484b4c889
victorgloria <victor.gloria@hotmail.com>
2024-04-04 19:57:11 -0500

    fixed photos file link

--- src/data/portfolio.ts
+++ src/data/portfolio.ts
@@ -8,19 +8,19 @@ interface Project {
 const portfolio: Project[] = [
     {
         title: 'Unity Game',
-        imgUrl: "public/2024-04-04.png",
+        imgUrl: "/2024-04-04.png",
         stack: ['C#', 'Github', 'Animation'],
         link: 'https://github.com/victorgloria/Final-Project'
     },
     {
         title: 'API RESTful APP',
-        imgUrl: "public/2024-04-04 (2).png",
+        imgUrl: "/2024-04-04 (2).png",
         stack: ['PHP', 'HTML', 'SQLite'],
         link: 'https://github.com/victorgloria/API-Project'
     },
     {
         title: 'About Me Website',
-        imgUrl: "public/2024-04-04 (1).png",
+        imgUrl: "/2024-04-04 (1).png",
         stack: ['Javascript', 'CSS', 'HTML'],
         link: 'https://github.com/victorgloria/Web-Technologies-project'
     },
